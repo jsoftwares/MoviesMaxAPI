a36eb729939d5c222d7eb62b22e32e3d46b833a9
Jeffrey Onochie <jeff.ict@gmail.com>
2022-05-28 16:27:00 +0100

    add Movie Post action, movieCreationDTO  and automapper configs

--- MoviesMaxAPI/Controllers/MovieController.cs
+++ MoviesMaxAPI/Controllers/MovieController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MoviesMaxAPI.DTOs;
+using MoviesMaxAPI.Entities;
 using MoviesMaxAPI.Helpers;
 
 namespace MoviesMaxAPI.Controllers
@@ -11,6 +13,7 @@ namespace MoviesMaxAPI.Controllers
         private readonly ApplicationDbContext db;
         private readonly IMapper mapper;
         private readonly IFileStorageService fileStorageService;
+        private readonly string folderName = "movies";
 
         public MovieController(ApplicationDbContext applicationDbContext, IMapper mapper, IFileStorageService fileStorageService)
         {
@@ -18,9 +21,34 @@ namespace MoviesMaxAPI.Controllers
             this.mapper = mapper;
             this.fileStorageService = fileStorageService;
         }
-        public IActionResult Index()
+        public async Task<ActionResult> Post([FromForm] MovieCreationDTO movieCreationDTO)
         {
-            return View();
+            var movie = mapper.Map<Movie>(movieCreationDTO);
+            
+            if (movieCreationDTO.Poster != null)
+            {
+                movie.Poster = await fileStorageService.SaveFile(folderName, movieCreationDTO.Poster);
+            }
+
+            AnnotateActorsOrder(movie);
+            db.Add(movie);
+            await db.SaveChangesAsync();
+
+            return NoContent();
         }
+
+        //Anotate the orders of the actors; modify d Order property of the MoviesActors entity so that so that we can save in DB
+        // d order in which they should appear in the movie details page. Here we are saving it in d order d actors came from the frontend.
+        private void AnnotateActorsOrder(Movie movie
+            )
+        {
+            if (movie.MovieActors != null)
+            {
+                for (int i = 0; i < movie.MovieActors.Count; i++)
+                {
+                    movie.MovieActors[i].Order = i;
+                }
+            }
+        } 
     }
 }
--- MoviesMaxAPI/Helpers/AutoMapperProfiles.cs
+++ MoviesMaxAPI/Helpers/AutoMapperProfiles.cs
@@ -17,12 +17,58 @@ namespace MoviesMaxAPI.Helpers
             CreateMap<ActorDTO, Actor>().ReverseMap();
             CreateMap<ActorCreationDTO, Actor>()
                 .ForMember(x=> x.Picture, options=> options.Ignore());
+            
             CreateMap<MovieTheatre, MovieTheatreDTO>()
                 .ForMember(x => x.Latitude, dto => dto.MapFrom(prop => prop.Location.Y))
                 .ForMember(x=> x.Longitude, dto => dto.MapFrom(prop => prop.Location.X));
             CreateMap<MovieTheatreCreationDTO, MovieTheatre>()
                 .ForMember(x => x.Location, x => x.MapFrom(dto => 
                 geometryFactory.CreatePoint(new Coordinate(dto.Longitude, dto.Latitude))));
+
+            CreateMap<MovieCreationDTO, Movie>()
+                .ForMember(x => x.Poster, options => options.Ignore())
+                .ForMember(x => x.MovieGenres, options => options.MapFrom(MapMoviesGenres))
+                .ForMember(x => x.MovieTheatresMovie, options => options.MapFrom(MapMovieTheatresMovies))
+                .ForMember(x => x.MovieActors, options => options.MapFrom(MapMoviesActors));
+        }
+
+        private List<MoviesGenres> MapMoviesGenres(MovieCreationDTO movieCreationDTO, Movie movie)
+        {
+            var result = new List<MoviesGenres>();
+
+            if (movieCreationDTO.GenresIds == null) { return result; }
+
+            foreach (var id in movieCreationDTO.GenresIds)
+            {
+                result.Add(new MoviesGenres() { GenreId = id });
+            }
+            return result;
+        }
+
+        private List<MovieTheatresMovies> MapMovieTheatresMovies(MovieCreationDTO movieCreationDTO, Movie movie)
+        {
+            var result = new List<MovieTheatresMovies>();
+
+            if (movieCreationDTO.MovieTheatresId == null) { return result;  }
+
+            foreach (var id in movieCreationDTO.MovieTheatresId)
+            {
+                result.Add(new MovieTheatresMovies() { MovieTheatreId = id });
+            }
+            return result;
+        }
+
+        private List<MoviesActors> MapMoviesActors(MovieCreationDTO movieCreationDTO, Movie movie)
+        {
+            var result = new List<MoviesActors>();
+
+            if (movieCreationDTO.Actors == null) { return result; }
+
+            foreach (var actor in movieCreationDTO.Actors)
+            {
+                result.Add(new MoviesActors() { ActorId = actor.Id, Character = actor.Character });
+            }
+            return result;
         }
     }
 }
