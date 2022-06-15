600a720c24bc4462d4936845966e50512a81ea85
Jeffrey Onochie <jeff.ict@gmail.com>
2022-06-15 11:51:59 +0100

    add controller for accounts to handle new account registeration & login

---
+++ MoviesMaxAPI/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MoviesMaxAPI.DTOs;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MoviesMaxAPI.Controllers
+{
+    /**In AccountController allows users to register and authenticate in our application. Also we will build d JWT that our frontend 
+     * will use to authenticate d user**/
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly IConfiguration configuration;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<AuthenticationResponse>> Create([FromBody] UserCredentials userCredentials)
+        {
+            /**Action will receive user credentials & return a response that contain d user JWT & expiration date**/
+            var user = new IdentityUser { UserName = userCredentials.Email, Email = userCredentials.Email};
+            var result = await userManager.CreateAsync(user, userCredentials.Password);
+
+            if (result.Succeeded)
+            {
+                return BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthenticationResponse>> Login([FromBody] UserCredentials userCredentials)
+        {
+            //isPersistent is used with cookies. lockoutOnFailure may be good for production to lockout an account if wrong password has been tried for a number of time
+            var result = await signInManager.PasswordSignInAsync(userCredentials.Email, userCredentials.Password, 
+                isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest("Incorrect credentials");
+            }
+        }
+
+        private AuthenticationResponse BuildToken(UserCredentials userCredentials)
+        {
+            //REM JWT is composed of 3 parts: payload data (contains collection of claims(user info) )
+            var claims = new List<Claim>()
+            {
+                new Claim("email", userCredentials.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["jwtkey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims, signingCredentials: creds);
+
+            return new AuthenticationResponse
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),    //give string representation of our Jwt Token
+                Expiration = expiration
+            };
+        }
+    }
+}
