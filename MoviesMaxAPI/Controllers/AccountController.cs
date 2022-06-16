510de449cb05f1281e64470863629c490c29dd3d
Jeffrey Onochie <jeff.ict@gmail.com>
2022-06-16 18:09:08 +0100

    add routes & actions to add and remove admin claim/role for a user

--- MoviesMaxAPI/Controllers/AccountController.cs
+++ MoviesMaxAPI/Controllers/AccountController.cs
@@ -37,7 +37,7 @@ namespace MoviesMaxAPI.Controllers
 
         [HttpGet("listUsers")]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "IsAdmin")]
-        public async Task<ActionResult<List<UserDTO>>> Get([FromQuery] PaginationDTO paginationDTO)
+        public async Task<ActionResult<List<UserDTO>>> GetListUsers([FromQuery] PaginationDTO paginationDTO)
         {
             var queryable = db.Users.AsQueryable();
             await HttpContext.InsertParametersPaginationInHeader(queryable);
@@ -46,6 +46,22 @@ namespace MoviesMaxAPI.Controllers
         }
 
         [HttpPost("makeAdmin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "IsAdmin")]
+        public async Task<ActionResult> MakeAdmin([FromBody] string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            await userManager.AddClaimAsync(user, new Claim("role", "admin"));
+            return NoContent();
+        }
+
+        [HttpPost("removeAdmin")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "IsAdmin")]
+        public async Task<ActionResult> RemoveAdmin([FromBody] string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            await userManager.RemoveClaimAsync(user, new Claim("role", "admin"));
+            return NoContent();
+        }
 
         [HttpPost("create")]
         public async Task<ActionResult<AuthenticationResponse>> Create([FromBody] UserCredentials userCredentials)
